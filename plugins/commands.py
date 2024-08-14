2dac4b57da6a471997dce162532390c34171e5bf
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-14 19:09:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,18 +27,13 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('⤬ 𝐀ᴅᴅ 𝐌ᴇ 𝐓ᴏ 𝐘ᴏᴜʀ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⤬', url=f'http://t.me/{temp.U_NAME}?startchannel=true')           
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        text=script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), 
-        await message.reply_text(
-        text=text,
-        reply_markup=reply_markup,
-        quote=True
+        reply_markup = InlineKeyboardMarkup(buttons)      
+        await message.reply_message(            
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
         )
-            if not await db.get_chat(message.chat.id):
-            total=await client.get_chat_members_count(message.chat.id)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
-            await db.add_chat(message.chat.id, message.chat.title)
-        return 
+        return
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
